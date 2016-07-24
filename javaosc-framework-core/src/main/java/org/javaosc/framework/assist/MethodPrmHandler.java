8cee96d949916929fcdb1a05718ef5164de507d4
dylan_tao <taodizhou@foxmail.com>
2016-07-25 00:45:52 +0800

    check framework's status

--- javaosc-framework-core/src/main/java/org/javaosc/framework/assist/MethodPrmHandler.java
+++ javaosc-framework-core/src/main/java/org/javaosc/framework/assist/MethodPrmHandler.java
@@ -5,7 +5,6 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
