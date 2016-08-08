d88f65a2598beb80a05d0afdf984aa9a6f83e7cb
dylan_tao <taodizhou@foxmail.com>
2016-08-09 01:03:38 +0800

    datasource match

--- javaosc-framework-core/src/main/java/org/javaosc/framework/jdbc/ConnectionHandler.java
+++ javaosc-framework-core/src/main/java/org/javaosc/framework/jdbc/ConnectionHandler.java
@@ -3,15 +3,10 @@ package org.javaosc.framework.jdbc;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import org.javaosc.framework.assist.ClassHandler;
-import org.javaosc.framework.assist.PropertyConvert;
 import org.javaosc.framework.constant.Constant;
-import org.javaosc.framework.context.ConfigurationHandler;
+import org.javaosc.framework.jdbc.pool.DataSourceMatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 /**
@@ -30,27 +25,7 @@ public class ConnectionHandler {
 	private static ThreadLocal<Connection> connManger = new ThreadLocal<Connection>();
 	
 	public static void init(){
-		String poolName = ConfigurationHandler.getDataSourceName();
-		if(poolName.indexOf("java:")==0){ //tomcat jdbc
-			try {
-				Context c = new InitialContext();
-				ds = (DataSource)c.lookup(poolName);
-			} catch (NamingException e) {
-				log.error(Constant.JAVAOSC_EXCEPTION, e);
-			}
-		}else{ //jdbc pool
-			Class<?> poolObj = ClassHandler.load(poolName);
-			if(poolObj!=null){
-				try {
-//					Object bean = poolObj.newInstance();
-//					BeanUtils.populate(bean, Configuration.getPoolParam());
-//					ds = (DataSource)bean;
-					ds = (DataSource) PropertyConvert.convert(ConfigurationHandler.getPoolParam(), poolObj, null);
-				} catch (Exception e) {
-					log.error(Constant.JAVAOSC_EXCEPTION, e);
-				} 
-			}
-		}
+		ds = DataSourceMatch.get();
 		if(ds!=null){
 			log.info("initializing the data source connection ~");
 			initialzeConnection();
---
+++ javaosc-framework-core/src/main/java/org/javaosc/framework/jdbc/pool/DataSourceMatch.java
@@ -0,0 +1,44 @@
+package org.javaosc.framework.jdbc.pool;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.javaosc.framework.assist.PropertyConvert;
+import org.javaosc.framework.constant.Constant;
+import org.javaosc.framework.context.ConfigurationHandler;
+import org.javaosc.framework.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataSourceMatch {
+	
+	private static final Logger log = LoggerFactory.getLogger(DataSourceMatch.class);
+	
+	public static DataSource get(){
+		DataSource ds = null; 
+		String dataSourceName = ConfigurationHandler.getDataSourceName();
+		if(StringUtil.isBlank(dataSourceName)){
+			log.error("pool.dataSource must be not null!");
+		}
+		
+		if(dataSourceName.indexOf("java:")==0){ //tomcat jdbc
+			try {
+				Context c = new InitialContext();
+				ds = (DataSource)c.lookup(dataSourceName);
+			} catch (NamingException e) {
+				log.error(Constant.JAVAOSC_EXCEPTION, e);
+			}
+		}else if(dataSourceName.indexOf(".c3p0.")>0){ //c3p0 pool
+			C3p0Handler c3p0Handler = null;
+			try {
+				c3p0Handler = PropertyConvert.convert(ConfigurationHandler.getPoolParam(), C3p0Handler.class, null);
+				ds = c3p0Handler.getDataSource();
+			} catch (Exception e) {
+				log.error(Constant.JAVAOSC_EXCEPTION, e);
+			} 	
+		}
+		return ds;
+	}
+}
