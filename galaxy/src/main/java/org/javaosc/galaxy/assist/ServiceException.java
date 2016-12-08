a83c783e358f1ab5b0d25ebd420fd488dd9423f2
dylan_tao <taodizhou@foxmail.com>
2016-12-09 01:09:42 +0800

    add service exception

---
+++ galaxy/src/main/java/org/javaosc/galaxy/assist/ServiceException.java
@@ -0,0 +1,31 @@
+package org.javaosc.galaxy.assist;
+
+/**
+ * 
+ * @description
+ * @author Dylan Tao
+ * @date 2014-09-09 Copyright 2014 Javaosc Team. All Rights Reserved.
+ */
+public class ServiceException extends RuntimeException {
+	
+	private static final long serialVersionUID = 8566978699237041746L;
+	
+	private long code;
+
+    public ServiceException(long code, String message) {
+        this(code, message, null);
+    }
+
+    public ServiceException(long code, Throwable cause) {
+        this(code, null, cause);
+    }
+
+    public ServiceException(long code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+    
+    public Long getErrorCode() {
+        return this.code;
+    }
+}
